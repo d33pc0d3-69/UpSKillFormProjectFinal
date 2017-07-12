b52cc87f8f6fdf9541bff71dd47b9e5a49530778
Robs <adrieaguilar@gmail.com>
2017-07-12 11:43:39 +0800

    Updated as of July 7 2017

--- assets/javascripts/core-js.js
+++ assets/javascripts/core-js.js
@@ -1,15 +1,18 @@
+if(localStorage.getItem("data")){
+
+	$("#data-container").addClass("hide");
+	$(".end-message").addClass("ack-message").text("THANK YOU FOR SIGNING!");
+	
+	}
+
 $("#btn-finish").click(function(){
 
-localStorage.setItem("data", "true");
+	localStorage.setItem("data", "true");
 
-$("#data-container").addClass("hide");
+	$("#data-container").addClass("hide");
+		
+	$(".end-message").addClass("ack-message").text("THANK YOU FOR SIGNING!");
 
-
-
-
-if(localStorage.getItem("data")) {
-
-$("#data-container").addClass("hide");
+});
 
 
-}

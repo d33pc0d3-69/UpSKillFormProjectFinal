a96b04118b66be57c0f92657896c1ee506c29531
Robs <adrieaguilar@gmail.com>
2017-07-13 13:08:05 +0800

    added validation control

--- assets/javascripts/core-js.js
+++ assets/javascripts/core-js.js
@@ -1,18 +1,33 @@
-if(localStorage.getItem("data")){
-
-	$("#data-container").addClass("hide");
-	$(".end-message").addClass("ack-message").text("THANK YOU FOR SIGNING!");
 	
-	}
+	if(localStorage.getItem("data")){
 
-$("#btn-finish").click(function(){
-
-	localStorage.setItem("data", "true");
-
-	$("#data-container").addClass("hide");
+		$("#data-container").addClass("hide");
+		$(".end-message").addClass("ack-message").text("THANK YOU FOR SIGNING!");
 		
-	$(".end-message").addClass("ack-message").text("THANK YOU FOR SIGNING!");
+		}
 
+$(".finish").click(function(){
+
+	$.ajax({
+
+		data: $("#formdata").serialize(),
+		type: "POST",
+		dataType: "json",
+
+		success: function(data){
+
+		localStorage.setItem("data", "true");
+		$("#data-container").addClass("hide");
+		$(".end-message").addClass("ack-message").text("THANK YOU FOR SIGNING!");
+		console.log(data);
+		},
+
+		error: function(){
+
+			alert("ERROR!!!");
+			console.log(data);
+
+		}
+
+	});
 });
-
-
--- assets/javascripts/formanimate.js
+++ assets/javascripts/formanimate.js
@@ -1,35 +1,39 @@
-$(".next").click(function(e){
+    $(".next").click(function(e){
 
-	e.preventDefault();
+        e.preventDefault();
 
+        if($("#formdata").valid()) {
+             current_fs = $(this).parent();
+             next_fs = $  (this).parent().next();
 
-     current_fs = $(this).parent();
-     next_fs = $  (this).parent().next();
-
-     $("#ul-prog li").eq($("fieldset").index(next_fs)).addClass("active");
-     $("article").eq($("fieldset").index(next_fs)).addClass("show");
-
-
-    next_fs.hide();
-    current_fs.hide("slide", { direction: "right" }, function() {
-        next_fs.show("slide", { direction: "left" });
-    });
-    
-});
+             $("#ul-prog li").eq($("fieldset").index(next_fs)).addClass("active");
+             $("article").eq($("fieldset").index(next_fs)).addClass("show");
 
 
-$(".back").click(function(){
-
-   
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-
-
-    $("#ul-prog li").eq($("fieldset").index(current_fs)).removeClass("active");
-    $("#art").eq($("fieldset").index(current_fs)).html("hide");
-
-    current_fs.hide("slide", { direction: "left" }, function() {
-        previous_fs.show("slide", { direction: "right" });
+            next_fs.hide();
+            current_fs.hide("slide", { direction: "right" }, function() {
+                next_fs.show("slide", { direction: "left" });
+            });
+        }
+        
     });
 
-});
+
+
+
+    $(".back").click(function(){
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+
+
+        $("#ul-prog li").eq($("fieldset").index(current_fs)).removeClass("active");
+        $("#art").eq($("fieldset").index(current_fs)).html("hide");
+
+        current_fs.hide("slide", { direction: "left" }, function() {
+            previous_fs.show("slide", { direction: "right" });
+        });
+
+    });
+
+
+
